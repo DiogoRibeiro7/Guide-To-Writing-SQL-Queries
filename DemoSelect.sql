f20723bc211bf37f88219e09e12a591afeceec09
Diogo Ribeiro <diogo_dj@hotmail.com>
2019-04-15 22:46:53 +0100

    Update DemoSelect.sql

--- DemoSelect.sql
+++ DemoSelect.sql
@@ -3,11 +3,6 @@ select
 from
 	[dbo].[Customer]
 	
-/* ************************************************************************************************
 
-	Hey there, As you are keen to learn SQL in this course, then you are probably are keen to
-	be more expert at SQL too, checkout my other courses
-	
-* ************************************************************************************************/ 
 
 	
