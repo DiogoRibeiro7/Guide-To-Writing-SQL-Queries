f3172c165fd1e211a955cc820bab3702944103dd
Diogo Ribeiro <diogo_dj@hotmail.com>
2019-04-15 23:26:05 +0100

    Update SelectSumFunctionDemo.sql

--- SelectSumFunctionDemo.sql
+++ SelectSumFunctionDemo.sql
@@ -1,9 +1,3 @@
-/* ************************************************************************************************
-
-	Hey there, As you are keen to learn SQL in this course, then you are probably are keen to
-	be more expert at SQL too, checkout my other courses
-	
-* ************************************************************************************************/ 
 
 -- Demo SQL Sum() function
 
