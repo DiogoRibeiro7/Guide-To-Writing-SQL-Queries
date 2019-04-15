5c366674847fa319964aa56019aabc7adef328de
Diogo Ribeiro <diogo_dj@hotmail.com>
2019-04-15 23:27:06 +0100

    Update The union clause.sql

--- The union clause.sql
+++ The union clause.sql
@@ -1,21 +1,4 @@
-/* ************************************************************************************************
 
-	Hey there, As you are keen to learn SQL in this course, then you are probably are keen to
-	be more expert at SQL too, as a reward you can enrol in my advanced SQL course for just 
-	$22 using the below coupon.
-	
-	SQL for PUNK ANALYTICS will take you from the intro we cover in Hitch Hikers to more be
-    more expert and smarter at SQL, and I use the same real world teaching methods too, so many 
-	students writing in telling me they really like my teaching approach which make it easier for
-	them to learn.
-
-	From what they say, no one else in UDEMY teaches this way...!
-
-	See you in the experts course :) 
-	
-	https://www.udemy.com/sql-for-punk-analytics/?couponCode=HalfPriceDiscount	
-	
-* ************************************************************************************************/ 
 
 -- What the heck is a union ?
 
